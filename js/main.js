b7d39165eb807e9794c23db370fc01e0765209bc
James Lee <jamesr2@Jamess-MacBook-Pro.local>
2017-06-22 12:07:47 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry, try again.");
+    }
+  }
+  
+var flipCard = function (cardId) {
+  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped" + cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
 
-var cardone = cards[0];
-var cardtwo = cards[2];
-cardsInPlay.push(cardone);
-cardsInPlay.push(cardtwo);
 
 
-console.log("User flipped" + " " + cardone);
-console.log("User flipped" + " " + cardtwo);
-
-if (cardsInPlay.length === 2) {
+/*
+if (cardsInPlay.length === 2)
   if (cardsInPlay[0] === cardsInPlay[1])
     alert("You found a match!");
-  else
+ else
     alert("Sorry try again.");
-}
+    */
