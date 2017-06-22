e07d45a819c4ab2ca5d260c3fbbc7f6dce168227
James Lee <jamesr2@Jamess-MacBook-Pro.local>
2017-06-22 16:14:37 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank:"queen",
+    suit:"heart",
+    cardImage:"images/queen-of-hearts.png"
+  },
+  {
+    rank:"queen",
+    suit:"diamonds",
+    cardImage:"images/queen-of-diamonds.png"
+  },
+  {
+    rank:"king",
+    suit:"heart",
+    cardImage:"images/king-of-hearts.png"
+  },
+  {
+    rank:"king",
+    suit:"diamonds",
+    cardImage:"images/king-of-diamonds.png"
+  },
+]
+
+
+
 var cardsInPlay = [];
+
 var checkForMatch = function () {
   if (cardsInPlay[0] === cardsInPlay[1]) {
         console.log("You found a match!");
@@ -7,13 +32,18 @@ var checkForMatch = function () {
         console.log("Sorry, try again.");
     }
   }
-  
+
 var flipCard = function (cardId) {
-  cardsInPlay.push(cards[cardId]);
-  console.log("User flipped" + cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("User flipped" + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 }
+
 flipCard(0);
 flipCard(2);
+
+
 
 
 
